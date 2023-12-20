20b22dae777fdbf9eb6ea6a7783ff059c4197f44
Melaka36 <melaka36pathiranagama@gmail.com>
2023-12-20 19:38:06 +0530

    Interfaces recap

---
+++ src/com/designPatterns/TaxCalculator2019.java
@@ -0,0 +1,12 @@
+package com.designPatterns;
+
+public class TaxCalculator2019 implements TaxCalculator{
+
+    @Override
+    public float calculateTax() {
+        return 1;
+    }
+    public float insuaranceTax(){
+        return 7.0f;
+    }
+}
